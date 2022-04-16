b397042ccdf3310f01b30f61364615d6c458e995
CelesTech03 <celeste.urena@stu.bmcc.cuny.edu>
2022-04-15 20:09:58 -0400

    RegisterTest

---
+++ UnionApp/Pods/Target Support Files/Pods-UnionApp/Pods-UnionApp-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_UnionApp : NSObject
+@end
+@implementation PodsDummy_Pods_UnionApp
+@end
---
+++ UnionApp/UnionApp/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  HomeViewController.swift
+//
+
+import Foundation
+import UIKit
+import Firebase
+
+class HomeViewController:UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func handleLogout(_ sender:Any) {
+        try! Auth.auth().signOut()
+        self.dismiss(animated: false, completion: nil)
+    }
+}
